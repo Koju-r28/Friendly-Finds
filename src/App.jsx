792d42ae9dd76abf689aeb80059f924aaa5059e0
sudha-258 <karkisudha21@gmail.com>
2025-11-28 21:31:48 +0545

    Login Page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginSignup from './Components/LoginSignup/LoginSignup.jsx'; // adjust path if needed
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Login Page</h1>
+      <img src="/vite.svg" alt="Vite Logo" />
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
