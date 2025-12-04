153417360b4248acf8196a057137c249e0f42e25
kojur28 <kojur28@gmail.com>
2025-12-04 11:20:24 +0545

    changes in the navbar

--- frontend-vite/src/components/Navbar/Navbar.jsx
+++ frontend-vite/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from "react";
-import "./Navbar.css";
-import logo from "../Assets/logo.png";
-import cart_icon from "../Assets/cart_icon.png";
+  import React from "react";
+  import "./Navbar.css";
+  import logo from "../Assets/logo.png";
+  import cart_icon from "../Assets/cart_icon.png";
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div classname="nav-logo">
-        <img src={logo} alt="logo"/>
-      </div>
-      <ul className="nav-menu">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-          <li>Cart</li>
-        
-      </ul>
-      
-      <button className="login-btn">Login</button>
-    </nav>
-  );
-};
+  const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <div className="nav-logo">
+          <img src={logo} alt="logo" />
+        </div>
 
-export default Navbar;
+        <ul className="nav-menu">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <img src={cart_icon} alt="cart" className="cart-icon" />
+          </li>
+        </ul>
+
+        <button className="login-btn">Login</button>
+      </nav>
+    );
+  };
+
+  export default Navbar;
